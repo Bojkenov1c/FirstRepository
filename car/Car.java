159070858284e20480ec2e1929e90eab68fea405
Bojkenov1c <bfurkan67@hotmail.com>
2018-09-18 23:55:31 +0300

    car

---
+++ car/Car.java
@@ -0,0 +1,29 @@
+package car;
+public class Car extends Engine{
+    public static void main(String[] args) {
+        Car mercedes_benz =new Car();
+        mercedes_benz.engine_power=245;
+        mercedes_benz.cylinder=4;
+        mercedes_benz.engine_capacity=1991;
+        mercedes_benz.max_speed=250;
+        mercedes_benz.rim_prints=17;
+        
+        Car bmw_5=new Car();
+        bmw_5.engine_power=218;
+        bmw_5.engine_capacity=1995;
+        bmw_5.max_speed=240;
+        bmw_5.cylinder=4;
+        bmw_5.rim_prints=17;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
---
+++ car/Engine.java
@@ -0,0 +1,8 @@
+package car;
+public class Engine extends Wheel {
+    int engine_power;
+    int cylinder;
+    int engine_capacity;
+}
+
+
