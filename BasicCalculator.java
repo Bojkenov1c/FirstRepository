9ec953c123f463048f30ba188897e6f9f06bfbf0
Bojkenov1c <bfurkan67@hotmail.com>
2018-09-20 12:59:06 +0300

    basiccalculator

---
+++ BasicCalculator.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class BasicCalculator {
+    public static void main(String[] args) {
+        Scanner x = new Scanner(System.in);
+        int birinci_sayi,ikinci_sayi;
+        System.out.println("Type Number 1");
+        birinci_sayi=x.nextInt();
+        System.out.println("Type Number 2");
+        ikinci_sayi=x.nextInt();
+        int iþlem,sonuç;
+        System.out.println("Addition=1,Extraction=2,Ýmpact=3,Division=4");
+        iþlem=x.nextInt();
+        if(iþlem==1){
+            sonuç=birinci_sayi+ikinci_sayi;
+            System.out.println("Result:"+sonuç);
+        }
+        else if(iþlem==2){
+            sonuç=birinci_sayi-ikinci_sayi;
+            System.out.println("Result:"+sonuç);
+        }
+        else if(iþlem==3){
+            sonuç=birinci_sayi*ikinci_sayi;
+            System.out.println("Result:"+sonuç);
+        }
+        else if(iþlem==4){
+            sonuç=birinci_sayi/ikinci_sayi;
+            System.out.println("Result:"+sonuç);
+        }        
+    }
+}
